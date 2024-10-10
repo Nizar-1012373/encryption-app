76a0b2883c88f719fb74ce24ea9416fd5e6de9f9
Nizar-1012373 <113017403+Nizar-1012373@users.noreply.github.com>
2024-10-10 14:56:27 +0200

    printed out encrypted text

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import flask
 
 from flask import request, render_template, Flask
+from cryptography.fernet import Fernet
+import os
+from dotenv import dotenv_values, load_dotenv
 
+config = dotenv_values(".env")
+
+load_dotenv()
 app = Flask(__name__, template_folder="html")
+user_key = os.getenv("user_key")
+
+encrypt_key = Fernet(user_key)
 
 
 @app.route("/")
@@ -21,8 +30,8 @@ def login():
 def encrypt():
     text = request.form.get("text")
     if request.method == "POST":
-
-        print(text)
+        message = encrypt_key.encrypt(text.encode())
+        print(message)
         return render_template("encrypt.html")
     if request.method == "GET":
         print(text)
