fa9aae7f8f22abe489e3f83ed18f019edb6604e4
Nizar-1012373 <113017403+Nizar-1012373@users.noreply.github.com>
2024-10-13 19:57:02 +0200

    you van encrypt and decrypt data only login does not work that good

--- main.py
+++ main.py
@@ -1,17 +1,39 @@
 import flask
 
-from flask import request, render_template, Flask
+from flask import redirect, request, render_template, Flask
 from cryptography.fernet import Fernet
 import os
 from dotenv import dotenv_values, load_dotenv
+from flask_sqlalchemy import SQLAlchemy
 
+app = Flask(__name__, template_folder="html")
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    "sqlite:////Users/Nizar/OneDrive - Hogeschool Rotterdam/encryption-app/database1.db"
+)
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.app_context().push()
+db = SQLAlchemy()
+db.init_app(app)
 config = dotenv_values(".env")
 
 load_dotenv()
-app = Flask(__name__, template_folder="html")
+
 user_key = os.getenv("user_key")
 
+
 encrypt_key = Fernet(user_key)
+
+
+class users(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column((db.String(100)), nullable=False)
+    pass_word = db.Column((db.String(100)), nullable=False)
+
+    def __init__(self):
+
+        return f"{self.user_name},{ self.pass_word}"
 
 
 @app.route("/")
@@ -20,14 +42,36 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/register")
+@app.route("/register", methods=["GET", "POST"])
 def register():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if request.method == "POST":
 
-    return render_template("register.html")
+        user = users(user_name=username, pass_word=password)
+        db.session.add(user)
+        db.session.commit()
+        return render_template("register.html", user_name=username, pass_word=password)
+    if request.method == "GET":
+        return render_template("register.html", user_name=username, pass_word=password)
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
+    username = request.form.get("username")
+    password = request.form.get("password")
+    if request.method == "POST":
+        user = users.query.all()
+
+        for i in user:
+            print(i.user_name, i.pass_word)
+
+            if str(i.user_name) and str(i.pass_word) == username and password:
+                return redirect("/encrypt")
+
+    if request.method == "GET":
+
+        return render_template("login.html")
     return render_template("login.html")
 
 
@@ -35,18 +79,29 @@ def login():
 def encrypt():
     text = request.form.get("text")
     if request.method == "POST":
-        message = encrypt_key.encrypt(text.encode())
+        message = encrypt_key.encrypt(text.encode("utf-8"))
         print(message)
-        return render_template("encrypt.html")
+        return render_template("encrypt.html", message=message)
     if request.method == "GET":
         print(text)
         return render_template("encrypt.html")
 
 
-@app.route("/decrypt")
+@app.route("/decrypt", methods=["GET", "POST"])
 def decrypt():
-    return render_template("decrypt.html")
+    text = request.form.get("text")
+    if request.method == "POST":
+        message2 = encrypt_key.decrypt(text).decode("utf-8")
+
+        print(type(message2))
+        return render_template("decrypt.html", message2=message2)
+    if request.method == "GET":
+        print(text)
+
+        return render_template("decrypt.html")
 
 
 if __name__ == "__main__":
+
+    db.create_all()
     app.run(debug=True)
